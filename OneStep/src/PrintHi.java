e5e2466b430b9e6490e13113287820ff91aef81a
sumitagrawal4548@gmail.com <sum@89AGR>
2016-07-13 22:21:53 +0530

    initial check in for One Step

---
+++ OneStep/src/PrintHi.java
@@ -0,0 +1,24 @@
+
+public class PrintHi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   String s = new String("s111111\"1111umit");
+	   
+	   
+	   
+	   int counter = 0;
+	   for( int i=0; i<s.length(); i++ ) {
+	       if( s.charAt(i) == '"' ) {
+	           counter++;
+	       } 
+	       
+	   }
+	   System.out.println(counter);
+	   System.out.println("comminting the first change to git local");
+	}
+
+}
