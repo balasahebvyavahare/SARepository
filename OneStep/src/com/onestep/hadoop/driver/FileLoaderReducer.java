1e42732ec82d4ec15891cbafca0cce8045edeec4
sumitagrawal4548@gmail.com <sum@89AGR>
2016-08-16 08:22:21 +0530

    Changes

---
+++ OneStep/src/com/onestep/hadoop/driver/FileLoaderReducer.java
@@ -0,0 +1,30 @@
+package com.onestep.hadoop.driver;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class FileLoaderReducer extends
+		Reducer<Text, Text, Text, Text> {
+
+	private Text totalWordCount = new Text();
+
+	@Override
+	public void reduce(final Text key, final Iterable<Text> values,
+			final Context context) throws IOException, InterruptedException {
+
+		String sum = "not found" ;
+		Iterator<Text> iterator = values.iterator();
+
+		while (iterator.hasNext()) {
+			sum = iterator.next().toString();
+		}
+
+		
+		// context.write(key, new IntWritable(sum));
+		context.write(new Text(""), new Text(sum));
+	}
+}
---
+++ OneStep/src/j_completereference/ClassDemo.java
@@ -0,0 +1,25 @@
+package j_completereference;
+
+
+public class ClassDemo {
+
+	static int a = 0;
+	int b = 5;
+
+	public void printa() {
+		System.out.println("The Value of a is : " + a);
+	}
+	
+	public static void staticMethodDemo()
+	{
+		System.out.println("I am in static and the value of a is :" + a );
+		
+	}
+
+	public static void main(String[] args) {
+
+		ClassDemo obj = new ClassDemo();
+		obj.printa();
+	}
+
+}
