ffe4c913eadae97f85809ad16805f83122873935
sumitagrawal4548@gmail.com <sum@89AGR>
2016-09-05 11:09:18 +0530

    ScalaFirstProgram

---
+++ OneStep/src/ScalaPrograms/HelloWord.scala
@@ -0,0 +1,10 @@
+package ScalaPrograms
+
+object HelloWorld {
+   /* This is my first java program.  
+   * This will print 'Hello World' as the output
+   */
+   def main(args: Array[String]) {
+      println("Hello, world!") // prints Hello World
+   }
+}
