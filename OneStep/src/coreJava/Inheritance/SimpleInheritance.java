fb78a93d74e9ce95233ac6d01c979a08a59c40c6
sumitagrawal4548@gmail.com <sum@89AGR>
2016-09-19 08:00:11 +0530

    Test123

---
+++ OneStep/src/coreJava/Inheritance/SimpleInheritance.java
@@ -0,0 +1,36 @@
+package coreJava.Inheritance;
+
+class A{
+	int i = 5, j = 7 ;
+	
+	void showIJ(){
+		System.out.println("I and J in A " + i + "  " + j );
+	}
+	
+}
+
+class B extends A{
+	
+	int k = 10;
+	
+	void showIJK(){
+		System.out.println("I and J and K in A " + i + "  " + j + " " + k);
+	}
+}
+
+
+public class SimpleInheritance {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		B b = new B();
+		int a = new A().hashCode();
+	
+		System.out.println("hashcode  " + a);
+		
+	}
+
+}
