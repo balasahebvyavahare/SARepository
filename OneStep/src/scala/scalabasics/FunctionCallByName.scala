2354fd07481e2605557c7e34f5b925f7aa50e112
sumitagrawal4548@gmail.com <sum@89AGR>
2016-09-19 16:14:12 +0530

    Functions and Loops

---
+++ OneStep/src/scala/scalabasics/FunctionCallByName.scala
@@ -0,0 +1,27 @@
+package scala.scalabasics
+
+/*
+ * Scala has both functions and methods and we use the terms method and function interchangeably with a minor difference.
+ *  A Scala method is a part of a class which has a name, 
+ *  a signature, optionally some annotations, 
+ *  and some bytecode where as a function in Scala is a complete object which can be assigned to a variable. In other words,
+ *   a function, which is defined as a member of some object, is called a method.
+ * 
+ */
+
+
+object FunctionCallByName {
+  def main(args: Array[String]) {
+    delayed(time());
+  }
+
+  def time() = {
+    println("Getting time in nano seconds")
+    System.nanoTime
+  }
+  def delayed(t: => Long) = {
+    println("In delayed method")
+    println("Param: " + t)
+  }
+
+}
---
+++ OneStep/src/scala/scalabasics/InfiniteLoopScala.scala
@@ -0,0 +1,16 @@
+package scala.scalabasics
+
+object InfiniteLoopScala {
+  
+    def main(args: Array[String]) {
+      var a = 10
+      // An infinite loop.
+      while ( true) {
+       //  a += 1
+        println("Value of a: " + a)
+        
+        
+  
+    }
+  }
+}
