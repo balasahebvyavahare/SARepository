7c69b61c10c8d7b3d4b873d54cd39b20d683df88
sumitagrawal4548@gmail.com <sum@89AGR>
2016-09-30 11:05:31 +0530

    Removing Java hadoop code from scala workspace

--- OneStep/src/j_completereference/Example.java
+++
@@ -1,8 +0,0 @@
-package j_completereference;
-
-class Example {
-	// Your program begins with a call to main().
-	public static void main(String args[]) {
-		System.out.println("This is a simple Java program.");
-	}
-}
